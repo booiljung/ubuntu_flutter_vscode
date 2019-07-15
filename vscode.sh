ba8201eecd3f67b904cf723d9d82b1f2b72657c9
booiljung <booiljung@me.com>
2019-07-15 17:48:09 +0900

    vscode auto install

--- vscode.sh
+++ vscode.sh
@@ -5,3 +5,17 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode s
 sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install code -y # or code-insiders
+
+code --install-extension aksharpatel47.vscode-flutter-helper
+code --install-extension alexisvt.flutter-snippets
+code --install-extension AzMoza.aqueduct-helper
+code --install-extension Dart-Code.dart-code
+code --install-extension Dart-Code.flutter
+code --install-extension esskar.vscode-flutter-i18n-json
+code --install-extension everettjf.pubspec-dependency-search
+code --install-extension FelixAngelov.bloc
+code --install-extension gmlewis-vscode.flutter-stylizer
+code --install-extension gornivv.vscode-flutter-files
+code --install-extension luanpotter.dart-import
+code --install-extension oscarcs.dart-syntax-highlighting-only
+code --install-extension pflannery.vscode-versionlens
